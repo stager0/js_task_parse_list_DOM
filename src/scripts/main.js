3ade164fd73f8d910fa04a9e7411158ec23d429d
Andrii <andriishtaher@gmail.com>
2025-10-21 21:54:38 +0200

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,64 @@
 'use strict';
 
 // write code here
-function convertSalary(salaryStr){
-  return Number(salaryStr.match(/\d/g).join(''))
+function convertSalary(salaryStr) {
+  return Number(salaryStr.match(/\d/g).join(''));
 }
-
 
 function sortList(employeesList) {
   const employeeArr = [];
 
-  for (let employee of employeesList) {
-    let userSalary = employee.getAttribute('data-salary')
-    employeeArr.push([convertSalary(userSalary), employee])
+  for (const employee of employeesList) {
+    const userSalary = employee.dataset.salary;
+
+    employeeArr.push([convertSalary(userSalary), employee]);
   }
 
   const sortedEmployees = employeeArr.sort(
-    (a, b) => Number(a[0]) - Number(b[0])
+    (a, b) => Number(b[0]) - Number(a[0]),
   );
 
   const arrSortedEmployees = () => {
     const arrToReturn = [];
 
     for (const employee of sortedEmployees) {
-      arrToReturn.push(employee[1])
+      arrToReturn.push(employee[1]);
     }
+
     return arrToReturn;
-  }
+  };
 
-  return arrSortedEmployees();
-}
+  const sortedArr = arrSortedEmployees();
+  const parentObj = sortedArr[0].parentElement;
 
+  parentObj.innerHTML = '';
 
-function getEmployees(selectorToFind) {
-  const empList = document.querySelectorAll(selectorToFind)
-  let sortedEmpList = sortList(empList)
-  const parent = empList[0].parentElement
-  parent.innerHTML = '';
-
-  for (const employeeHtml of sortedEmpList) {
-    parent.appendChild(employeeHtml);
+  for (const employeeHtml of arrSortedEmployees()) {
+    parentObj.appendChild(employeeHtml);
   }
 }
 
-getEmployees('li')
+function getEmployees(employeesList) {
+  const parsedEmployeeArr = [];
+
+  for (const employee of employeesList) {
+    const objToAdd = {};
+
+    objToAdd['name'] = employee.textContent;
+    objToAdd['position'] = employee.dataset.position;
+    objToAdd['salary'] = employee.dataset.salary;
+    objToAdd['age'] = employee.dataset.age;
+
+    parsedEmployeeArr.push(objToAdd);
+  }
+
+  return parsedEmployeeArr;
+}
+
+const employeeList = document.querySelectorAll('li');
+
+sortList(employeeList);
+
+const sortedEmployeeList = document.querySelectorAll('li');
+
+getEmployees(sortedEmployeeList);
