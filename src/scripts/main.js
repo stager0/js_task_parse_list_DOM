e4360c7b399b3447d3bb724cf3bb89bf2d2951c7
Andrii <andriishtaher@gmail.com>
2025-10-21 21:23:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
 // write code here
+function convertSalary(salaryStr){
+  return Number(salaryStr.match(/\d/g).join(''))
+}
+
+
+function sortList(employeesList) {
+  const employeeArr = [];
+
+  for (let employee of employeesList) {
+    let userSalary = employee.getAttribute('data-salary')
+    employeeArr.push([convertSalary(userSalary), employee])
+  }
+
+  const sortedEmployees = employeeArr.sort(
+    (a, b) => Number(a[0]) - Number(b[0])
+  );
+
+  const arrSortedEmployees = () => {
+    const arrToReturn = [];
+
+    for (const employee of sortedEmployees) {
+      arrToReturn.push(employee[1])
+    }
+    return arrToReturn;
+  }
+
+  return arrSortedEmployees();
+}
+
+
+function getEmployees(selectorToFind) {
+  const empList = document.querySelectorAll(selectorToFind)
+  let sortedEmpList = sortList(empList)
+  const parent = empList[0].parentElement
+  parent.innerHTML = '';
+
+  for (const employeeHtml of sortedEmpList) {
+    parent.appendChild(employeeHtml);
+  }
+}
+
+getEmployees('li')
